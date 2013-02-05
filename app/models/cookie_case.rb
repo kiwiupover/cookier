bd86879d5ab2634f3dedc515914696cfa38e1eab
Dave Laird <David.Laird@citygridmedia.com>
2013-02-04 23:04:49 -0800

    added base 12 math

--- app/models/cookie_case.rb
+++ app/models/cookie_case.rb
@@ -2,4 +2,17 @@ class CookieCase < ActiveRecord::Base
   attr_accessible :name, :quantity, :site_sale_id
 
   belongs_to :site_sale
+
+  def cases_count
+    "#{cases_base_ten.to_i} cases and boxes #{boxes_left_over}"
+  end
+
+  def cases_base_ten
+    quantity.to_i / 12.0
+  end
+
+  def boxes_left_over
+    (12*(cases_base_ten - cases_base_ten.to_i)).round
+  end
+
 end
